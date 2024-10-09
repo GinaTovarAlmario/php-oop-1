f1a8f077f3a367245c7486b24103cd02249eb57f
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-10-09 17:57:06 +0200

    added some style

--- index.php
+++ index.php
@@ -40,35 +40,28 @@ require_once __DIR__ . "../classes/Movie.php";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
+<body class="bg-dark">
     <main class="container">
-        <h1>Movie List</h1>
+        <h1 class="text-center mt-3 text-success">Movie List</h1>
         <div class="row">
-            <div class="col-12">
-                <ul>
-                    <?php foreach($movies as $movie) { ?>
-                        <li>
-                            <p>
+            <?php foreach($movies as $movie) { ?>
+                <div class="col-4 mb-3">
+                    <div class="card mt-3 text-dark bg-secondary-subtle" style="width: 300px; height:550px;">
+                        <img src="<?= $movie->cover;?>" style="width: 300px; height: 370px;">
+                        <div class="card-body">
+                            <h5 class="card-title">
                                 Titolo: <?= $movie->title; ?>
-                            </p>
-                            <p>
-                                Genere: <?= $movie->genre; ?>
-                            </p>
+                            </h5>
                             <p>
                                 Year: <?= $movie->year; ?>
                             </p>
-
-                            <?php if (isset($movie->cover)) { ?>
-                                <img src="<?= $movie->cover; ?>" style="width: 200px; height: auto" />
-                            <?php } ?>
-
                             <p>
                                 Film formattato: <?= $movie->getFormattedMovie(); ?>
                             </p>
-                        </li>
-                    <?php } ?>
-                </ul>
-            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
         </div>
     </main>
 </body>
