e3e83f3d418ec2c2dff68de7e6b759279dab9593
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-10-09 15:28:12 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php 
+
+// Oggi pomeriggio ripassate i primi concetti di classe, variabili e metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
+// è definita una classe ‘Movie’
+// => all'interno della classe sono dichiarate delle variabili d'istanza
+// => all'interno della classe è definito un costruttore
+// => all'interno della classe è definito almeno un metodo
+// vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+// Bonus 1:
+// Modificare la classe Movie in modo che accetti piú di un genere.
+// Bonus 2:
+// Creare un layout completo per stampare a schermo una lista di movies.
+// Facciamo attenzione all’organizzazione del codice, suddividendolo in appositi file e cartelle.
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="ginatovaralmario">
+    <meta name="project" content="php-oop">
+    <title>Document</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <main class="container">
+
+    </main>
+    
+</body>
+</html>
